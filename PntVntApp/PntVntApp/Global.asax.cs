27c3350c29766d45474e41fe701a1a88474e84a6
nano1209 <adrianure@gmail.com>
2015-05-20 19:47:42 -0600

    +javascript

--- PntVntApp/PntVntApp/Global.asax.cs
+++ PntVntApp/PntVntApp/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace PntVntApp
@@ -14,11 +15,13 @@ namespace PntVntApp
     {
         protected void Application_Start()
         {
+            //BundleTable.EnableOptimizations = true;
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
